7e489dd45a550fc7687c0c2dce1ace2c306b2ca8
Peter Ringelmann <peterringelmann@gmail.com>
2016-05-07 10:02:00 +0200

    Added image uploader to home page

--- wedding/views.py
+++ wedding/views.py
@@ -1,6 +1,11 @@
 import json
+import os
+import random
+import string
 
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 from django.http import HttpResponse
 
@@ -10,8 +15,10 @@ from django.core.urlresolvers import reverse
 from wedding import GMUSIC_API
 from gedeck.models import Invitation, Guest
 
+from .forms import UploadFileForm
+
 def home(request):
-    return render(request, 'wedding/home.html')
+    return render(request, 'wedding/upload.html')
 
 def check_rsvp(request):
     email = request.GET.get('email')
@@ -21,8 +28,8 @@ def check_rsvp(request):
         guest = Guest.objects.get(email=email)
     except:
         return render(request, 'wedding/home.html', {
-		      'alert': 'We did not find your email address on the guest list.'
-	    })
+              'alert': 'We did not find your email address on the guest list.'
+        })
 
     invite = Invitation.objects.filter(guests=guest).first()
     return redirect('invitation', invitation_ref=invite.ref)
@@ -67,3 +74,29 @@ def add_song(request):
     results = GMUSIC_API.add_songs_to_playlist('0391d1e9-9090-4a42-a511-f76066c50566', title);
 
     return HttpResponse('success');
+
+@csrf_exempt
+def upload(request):
+
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse('success');
+    else:
+        return HttpResponse('not allowed');
+    return HttpResponse('uplaod failed :()');
+
+def handle_uploaded_file(f):
+    name, extension = os.path.splitext(f.name)
+    fname = os.path.join(settings.MEDIA_ROOT, f.name)
+    counter = 0
+
+    while os.path.isfile(fname):
+        new_name = name + "_" + str(counter) + extension
+        fname = os.path.join(settings.MEDIA_ROOT, new_name)
+        counter += 1
+
+    with open(os.path.join(fname), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
